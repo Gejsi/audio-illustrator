a0470a999e2a58b04d8778ddf00dac9d093dabce
Gejsi <vgejsi@gmail.com>
2018-11-20 22:27:04 +0100

    Exported every component

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Illustrator } from './illustrator'
+import { LinearBars } from './linearBars'
+import { PulsingCircles } from './pulsingCircles'
+import { RadialBars } from './radialBars'
+
+export { LinearBars, PulsingCircles, RadialBars }
+export default Illustrator
